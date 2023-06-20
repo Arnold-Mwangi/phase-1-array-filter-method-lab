bd5048ed7d7a329a1a3b2253637ca8368408ff8e
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-06-20 15:31:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter(driver =>
+      driver.toLowerCase() === query.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch (drivers, letter){
+    return drivers.filter(driver => 
+        driver.startsWith(letter))
+  }
+
+  function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name)
+  }
